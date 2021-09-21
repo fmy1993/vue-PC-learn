3aa7ca27a9ca0050091f4be143f76e713041bcad
fmy1993 <fmy19930323@outlook.com>
2021-09-22 06:52:52 +0800

    update

--- src/settings.js
+++ src/settings.js
@@ -4,13 +4,13 @@
  * @Author: fmy1993
  * @Date: 2021-09-20 21:43:30
  * @LastEditors: fmy1993
- * @LastEditTime: 2021-09-21 16:48:36
+ * @LastEditTime: 2021-09-21 16:49:50
  */
 module.exports = {
   // 显示在浏览器的tab上
   title: 'Vue Admin Template',
 
-  /** 导航的第一行固定住
+  /** 导航的第一行固定住,不消失
    * @type {boolean} true | false
    * @description Whether fix the header
    */
